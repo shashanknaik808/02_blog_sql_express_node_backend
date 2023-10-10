388010d781a735a0e175eff3ec25b4ea2a77173a
Shashank Naik <shashanknaik808@gmail.com>
2023-10-10 14:21:32 +0530

    Setting up the Routes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-
+const userRouter = require("./routes/user-routes.js");
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/user", userRouter);
 
 
 
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,4 +4,6 @@ const express = require('express');
 const userRouter = express.Router();
 
 
+userRouter.post("/signup",signup);
+
 module.exports = userRouter;
