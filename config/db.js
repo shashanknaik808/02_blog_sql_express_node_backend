9bad5c896222e9974ac5b8ba338f4668f27fc23d
Shashank Naik <shashanknaik808@gmail.com>
2023-10-10 14:02:32 +0530

    Adding Port

--- config/db.js
+++ config/db.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
 
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
 
 module.exports = db;
