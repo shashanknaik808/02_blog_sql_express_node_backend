2124bf55f08f9b115d64f96dfb6e49968abaed5c
Shashank Naik <shashanknaik808@gmail.com>
2023-10-11 10:28:24 +0530

    Added blog controller & created getAllBlogs function

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose');
 const db = require('../config/db.js');
+
+module.exports.getAllBlogs = async (req, res, next) => {
+    let blogs;
+    try {
+        blogs = await Blog.find().populate("user");
+    }
+    catch (err) {
+        return console.log(err);
+    }
+    if (!blogs) {
+        return res.status(404).json({ message: "No Blogs Found" });
+    }
+    return res.status(200).json({ blogs });
+};
