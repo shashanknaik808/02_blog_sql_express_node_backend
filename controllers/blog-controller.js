9093664a3c9335a00b703b76908c84c037e7d8b9
Shashank Naik <shashanknaik808@gmail.com>
2023-10-11 10:47:25 +0530

    Creating addBlog Function

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -10,3 +10,27 @@ module.exports.getAllBlogs = async (req, res, next) => {
         }
     })
 };
+
+module.exports.addBlog = async (req, res, next) => {
+    const { title, description, image, id } = req.body;
+    db.query('SELECT * FROM USERS WHERE ID = ?', [id], (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        if (results.length === 0) {
+            return res.json({
+                message: 'Unable to find the user'
+            })
+        }
+        db.query('INSERT INTO BLOGS SET ?', { title: title, description: description, image: image, id: id }, (err, results) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(results);
+                return res.json({
+                    message: 'Blog added'
+                })
+            }
+        })
+    })
+};
