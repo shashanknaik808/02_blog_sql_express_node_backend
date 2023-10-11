3a700a8a0a4ef535e1744c37f764f19978abc108
Shashank Naik <shashanknaik808@gmail.com>
2023-10-11 10:38:42 +0530

    Using MiddleWare and Setting Up the Routes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const userRouter = require("./routes/user-routes.js");
+const blogRouter = require("./routes/blog-routes.js");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/user", userRouter);
-
+app.use("/api/blog", blogRouter);
 
 
 const PORT = process.env.PORT || 4000;
